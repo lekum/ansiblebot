a0017567ff87ee10de5cc5bf07760f7bd7f7250a
Alejandro Guirao <lekumberri@gmail.com>
2016-01-21 11:39:56 +0100

    fix(): Remove unnecesary line breaks

--- src/plugins/ansible-bot.py
+++ src/plugins/ansible-bot.py
@@ -9,4 +9,4 @@ class AnsibleBot(BotPlugin):
         """Run one-time Ansible commands"""
         cmd = " ".join(("venv/bin/ansible", args))
         prefix = "".join(("Executing `ansible ", args, "`:"))
-        return "\n\n\n\n".join((prefix, check_output(cmd, shell=True)))
+        return "\n".join((prefix, check_output(cmd, shell=True)))
